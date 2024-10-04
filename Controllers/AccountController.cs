effb86dd38743e8bb8daf7f760bd3e8a2bffc8ff
hai nguyen duy <91516568+HaiNguyenDuy123@users.noreply.github.com>
2024-10-04 13:18:58 +0700

    AccountController.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -38,40 +38,18 @@ public class AccountController(SignInManager<AppUser> signInManager, UserManager
         return View();
     }
 
-    [HttpPost]
-    public async Task<IActionResult> Register(RegisterVM model, string? returnUrl = null)
-    {
-        ViewData["ReturnUrl"] = returnUrl;
-        if (ModelState.IsValid)
-        {
-            AppUser user = new()
-            {
-                Name = model.Name,
-                UserName = model.Email,
-                Email = model.Email,
-                Address = model.Address
-            };
 
-            var result = await userManager.CreateAsync(user, model.Password!);
-
-            if (result.Succeeded)
-            {
-                await signInManager.SignInAsync(user, false);
-
-                return RedirectToLocal(returnUrl);
-            }
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
-            }
-        }
-        return View(model);
-    }
 
     public async Task<IActionResult> Logout()
     {
         await signInManager.SignOutAsync();
         return RedirectToAction("Index","Home");
     }
-
+    private IActionResult RedirectToLocal(string? returnUrl)
+    {
+        return !string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl)
+            ? Redirect(returnUrl)
+            : RedirectToAction(nameof(HomeController.Index), nameof(HomeController));
+    }
+}
 
